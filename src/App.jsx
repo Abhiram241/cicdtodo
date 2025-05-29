9e8d84ac73b256c5d095023d775b9dbf9cda3763
SumanBS <suman.bs200303@gmail.com>
2025-05-29 14:35:27 +0530

    hello abhiram

--- src/App.jsx
+++ src/App.jsx
@@ -166,7 +166,7 @@ function App() {
                   <div className=" text-center text-orange-500">
                     You seem to be lazy!
                   </div>
-                  <div>Start Tasks</div>
+                  <div>Start Planning</div>
                 </div>
               ) : (
                 todos.map((item) => {
